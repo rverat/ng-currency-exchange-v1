d38a4e7f1fa5a3c4aae0d760d2d269d67ea46487
azure_dev <thedevlair@gmail.com>
2024-04-27 12:23:39 -0500

    fix: error login

--- src/app/core/interceptor/token.interceptor.service.ts
+++ src/app/core/interceptor/token.interceptor.service.ts
@@ -3,22 +3,30 @@ import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/c
 import { Observable } from 'rxjs';
 import { finalize } from 'rxjs/operators';
 import { AuthService } from '../services/auth.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TokenInterceptorService implements HttpInterceptor{
 
-  constructor(private auth: AuthService) { }
+  constructor(private auth: AuthService,
+     private router: Router
+  ) { }
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     let authReq = req;
-    authReq = authReq.clone({
-      setHeaders : {
-        'Authorization': 'Bearer ' + this.auth.getToken()
-      }
-    });
+    if(this.router.url != '/login'){
+        authReq = authReq.clone({
+            setHeaders : {
+              'Authorization': 'Bearer ' + this.auth.getToken()
+            }
+          });
+    }
 
+    console.dir('authReq:::');
+    console.dir(authReq);
+    
     return next.handle(authReq);
   }
 }
